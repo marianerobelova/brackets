46b05cbce7ebf55fadf2232ca3eca08b77d8807e
”marianerobelova” <masha98-1998@mail.ru>
2023-02-27 20:46:45 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  bracketsConfig.forEach((config) => {
+    const openingBracket = config[0];
+
+    for (let i = 0; i < str.length; i++) {
+      let currEl = str[i];
+
+      if (config.includes(currEl)) {
+        
+        if (currEl === openingBracket) {
+          stack.push(currEl);
+        } else {
+
+          if (stack.length === 0) {
+            return false;
+          }
+
+          let topEl = stack[stack.length - 1];
+
+          if (topEl === openingBracket) {
+            stack.pop()
+          } else {
+            return false;
+          }
+
+        }
+      
+      }
+
+    }
+  });
+
+  return stack.length === 0
+  
+};
